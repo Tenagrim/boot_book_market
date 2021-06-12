e289f0590e502854e429eabdb60021633987be0d
Tenagrim <tenagrim@gmail.com>
2021-06-12 11:36:19 +0300

    initial commit

---
+++ src/main/java/com/market/book_market/BookMarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.book_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market/book_market/BookMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.book_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
