b66f84fbb90e23cd935d1380b6d7b06779aff15f
Tenagrim <tenagrim@gmail.com>
2021-06-13 15:22:12 +0300

    somemore documentation

--- src/main/java/com/market/book_market/controller/AuthController.java
+++ src/main/java/com/market/book_market/controller/AuthController.java
@@ -1,11 +1,17 @@
 package com.market.book_market.controller;
 
 import com.market.book_market.configuration.jwt.JwtProvider;
+import com.market.book_market.entity.Book;
 import com.market.book_market.entity.User;
 import com.market.book_market.requests.AuthResponse;
 import com.market.book_market.requests.AuthorizationRequest;
 import com.market.book_market.requests.RegistrationRequest;
 import com.market.book_market.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +29,12 @@ public class AuthController {
     @Autowired
     private JwtProvider jwtProvider;
 
+    @Operation(summary = "Register")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Registration", content = {
+                    @Content(mediaType = "text")
+            })
+    })
     @PostMapping("/signup")
     public String sginUp(@RequestBody RegistrationRequest registrationRequest)
     {
@@ -34,6 +46,13 @@ public class AuthController {
         return "OK";
     }
 
+    @Operation(summary = "Get auth token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get auth token", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthResponse.class))
+            })
+    })
     @PostMapping("/signin")
     public AuthResponse auth(@RequestBody AuthorizationRequest request) {
         User userEntity = userService.findByUsernameAndPassword(request.getUsername(), request.getPassword());
